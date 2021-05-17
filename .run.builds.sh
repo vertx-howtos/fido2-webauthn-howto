d64d2ea42b0642d4dcb4b48de669739fc9caf07b
Paulo Lopes <pmlopes@gmail.com>
2021-05-17 16:09:47 +0200

    Update to gh actions
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- .run.builds.sh
+++ .run.builds.sh
@@ -1,5 +1,4 @@
 #/usr/bin/env bash
 set -e
 
-mvn clean install
-./gradlew build
+./mvnw clean install
--- .run.jekyll.sh
+++ .run.jekyll.sh
@@ -1,5 +1,22 @@
 #/usr/bin/env bash
 set -e
 
+echo "⚙️  Building with Jekyll"
+gem install bundler
 bundle install
 bundle exec jekyll build
+
+echo "⚙️  Copying the files"
+rm -rf _gh_pages/*
+cp -R _site/* _gh_pages/
+mv _gh_pages/README.html _gh_pages/index.html || true
+
+echo "🚀 Commit and push"
+cd _gh_pages || exit
+git config --global user.email "howtos@vertx.io"
+git config --global user.name "Vert.x howtos"
+git add -A
+git commit -m "Deploy the how-to pages"
+git push origin gh-pages
+
+echo "✅ Done"
