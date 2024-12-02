9dd2f9bc74852e252420e448b3220c9188b4e124
Thomas Segismont <tsegismo@redhat.com>
2024-12-02 17:49:55 +0100

    Use GH Action to deploy pages

--- .run.builds.sh
+++ .run.builds.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
 
 ./mvnw clean install
--- .run.jekyll.sh
+++ .run.jekyll.sh
@@ -6,21 +6,4 @@ gem install bundler
 bundle install
 bundle exec jekyll build
 
-echo "⚙️  Copying the files"
-rm -rf _gh_pages/*
-cp -R _site/* _gh_pages/
-# include sources as they are linked from the howto
-cp -R src _gh_pages/
-cp pom.xml _gh_pages/
-# sometimes jekyll doesn't rename to index
-mv _gh_pages/README.html _gh_pages/index.html || true
-
-echo "🚀 Commit and push"
-cd _gh_pages || exit
-git config --global user.email "howtos@vertx.io"
-git config --global user.name "Vert.x howtos"
-git add -A
-git commit -m "Deploy the how-to pages"
-git push origin gh-pages
-
 echo "✅ Done"
